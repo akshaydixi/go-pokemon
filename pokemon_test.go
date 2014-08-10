1a0c8ee839b99295ae4af428b21b174f763ddb7d
Akshay Dixit <akshaydixi@gmail.com>
2014-08-10 03:30:34 -0700

    go fmt

--- pokemon_test.go
+++ pokemon_test.go
@@ -1,56 +1,54 @@
 package pokemon
 
 import (
-"testing"
+	"testing"
 )
 
 func Test_GamePokemonRed(t *testing.T) {
-  game, err := getGame("4")
-  if err != nil {
-    t.Error("Error: ",err)
-  } else {
-    t.Log("Passed: ",game.Name)
-  }
+	game, err := getGame("4")
+	if err != nil {
+		t.Error("Error: ", err)
+	} else {
+		t.Log("Passed: ", game.Name)
+	}
 }
 
 func Test_GameError(t *testing.T) {
-  game, err := getGame("1231")
-  if err != nil {
-    t.Log("Passed: ",err)
-  } else {
-    t.Error("Error: ",game)
-  }
+	game, err := getGame("1231")
+	if err != nil {
+		t.Log("Passed: ", err)
+	} else {
+		t.Error("Error: ", game)
+	}
 }
 
 func Test_PokemonCharizardByName(t *testing.T) {
-  pokemon, err := getPokemon("charizard")
-  if err != nil {
-    t.Error("Error: ",err)
-  } else if pokemon.Name != "Charizard" {
-    t.Error("Error: Wrong Pokemon???")
-  } else {
-    t.Log("Passed:",pokemon.Name, pokemon.National_id)
-  }
+	pokemon, err := getPokemon("charizard")
+	if err != nil {
+		t.Error("Error: ", err)
+	} else if pokemon.Name != "Charizard" {
+		t.Error("Error: Wrong Pokemon???")
+	} else {
+		t.Log("Passed:", pokemon.Name, pokemon.National_id)
+	}
 }
 
 func Test_PokemonCharizardById(t *testing.T) {
-  pokemon, err := getPokemon("6")
-  if err != nil {
-    t.Error("Error: ",err)
-  } else if pokemon.Name != "Charizard" {
-    t.Error("Error: Wrong Pokemon???")
-  } else {
-    t.Log("Passed: ",pokemon.Name, pokemon.National_id)
-  }
+	pokemon, err := getPokemon("6")
+	if err != nil {
+		t.Error("Error: ", err)
+	} else if pokemon.Name != "Charizard" {
+		t.Error("Error: Wrong Pokemon???")
+	} else {
+		t.Log("Passed: ", pokemon.Name, pokemon.National_id)
+	}
 }
 
 func Test_PokedexCallSuccess(t *testing.T) {
-  pokedex, err := getPokedex("1")
-  if err != nil {
-    t.Error("Error: ",err)
-  } else {
-    t.Log("Passed :",pokedex.Name," contains ",len(pokedex.Pokemon), " pokemon")
-  }
+	pokedex, err := getPokedex("1")
+	if err != nil {
+		t.Error("Error: ", err)
+	} else {
+		t.Log("Passed :", pokedex.Name, " contains ", len(pokedex.Pokemon), " pokemon")
+	}
 }
-
-
